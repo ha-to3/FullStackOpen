b1e926ac861b36e954d008a1bd4a7051759488fd
unknown <hadokun.jp@gmail.com>
2022-06-28 21:23:32 -0500

    Exercise 2.1-2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,47 +1,4 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-      {course.map(course => 
-        <div key={course.id}>
-        <Header name={course.name}/>
-        <Content parts={course.parts}/>
-        <Total parts={course.parts} /> 
-      </div> )}
-    </div>
-  )
-}
-const Header = ({ name }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-    </div>
-  )
-}
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(parts =>
-        <Part key={parts.id} parts={parts.name} exercises={parts.exercises} 
-      />)}
-    </div>
-  )
-}
-const Part = ({ parts, exercises }) => {
-  return (
-    <p> {parts} {exercises} </p>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total =
-    parts.reduce((sum,parts) => sum + parts.exercises, 0)
-
-  return (
-    <p>
-      Number of exercises {total}
-    </p>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const course =  [
