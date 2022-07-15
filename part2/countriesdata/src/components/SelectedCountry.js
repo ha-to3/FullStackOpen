6afdfb0041bc063cef569a4afef0c217ac350602
unknown <hadokun.jp@gmail.com>
2022-07-14 23:52:07 -0500

    2.12-2.14

--- part2/countriesdata/src/components/SelectedCountry.js
+++ part2/countriesdata/src/components/SelectedCountry.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 
 const SelectedCountry = ({ selectedCountry, setOneCountry }) => {
+  const [weather,setWeather] = useState({})
+  const api_key = process.env.REACT_APP_API_KEY
+  const lat = selectedCountry.latlng[0]
+  const lon = selectedCountry.latlng[1]
+  useEffect(() => {
+      axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`)
+      .then(res => {
+        setWeather(res.data)
+        console.log(res.data)
+        console.log(lat,lon)
+      })
+    }, []);
+
     const countryLanguage = Object.values(selectedCountry.languages)
     const handleClick = () => 
     {
